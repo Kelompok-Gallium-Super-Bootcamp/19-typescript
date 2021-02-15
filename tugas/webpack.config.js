03593de288743167bb88aa2baa6855534df206dd
Muhammad Choirudin Malik <77602202+choirmalik@users.noreply.github.com>
2021-02-15 14:56:29 +0700

    add initial configuration

--- tugas/webpack.config.js
+++ tugas/webpack.config.js
@@ -1,10 +1,14 @@
+const DotenvWebpackPlugin = require('dotenv-webpack');
 const path = require('path');
 
 module.exports = {
   entry: {
-    tasks: './webapp/src/tasks/main.js',
-    worker: './webapp/src/worker/main.js',
-    performance: './webapp/src/performance/main.js',
+    tasks: './src/tasks/main.js',
+    worker: './src/worker/main.js',
+    performance: './src/performance/main.js',
+    workerschema: './schema/worker.js',
+    taskschema: './schema/task.js',
+    performanceschema: './schema/performance.js',
   },
   output: {
     path: path.resolve(__dirname, 'www'),
@@ -15,11 +19,29 @@ module.exports = {
     contentBase: './webapp//www',
     port: 7000,
   },
+	plugins: [
+    new DotenvWebpackPlugin({
+      path: './.env',
+      safe: true,
+    }),
+  ],
+	resolve: {
+    extensions: ['.js', '.ts'],
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+			{
+        test: /\.yaml$/,
+        use: [{ loader: 'json-loader' }, { loader: 'yaml-loader' }],
+      },
+			{
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
       },
     ],
   },
